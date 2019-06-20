3dbe6fb73a1d635dc29ea9c25753a1f3f7c47f7b
wuhu <wuhu@itcast.cn>
2019-06-20 11:43:25 +0800

    添加了登陆中的表单组件，添加了验证规则，添加了样式，下一步实现登陆界面的细节，如添加图标

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
 // 引入路由模块
 import router from '@/router.js'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,11 +1,75 @@
 <template>
-    <div class="login">登陆</div>
+  <div class="login">
+    <div class="container">
+      <img src="../assets/avatar.jpg" alt class="avatar">
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" class="demo-ruleForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="login-btn">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
-
+  // 定义数据
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      //   添加验证规则
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  }
 }
 </script>
-<style lang="less" scoped>
 
+<style lang="less" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
 </style>

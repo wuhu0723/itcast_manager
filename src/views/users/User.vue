073d598656aa22051dad39e5abb5e9a036cd8982
wuhu <wuhu@itcast.cn>
2019-06-21 12:13:11 +0800

    user页面中添加表格结构，特别要注意添加了自定义模板，以便后期操作时获取到数据

--- src/views/users/User.vue
+++ src/views/users/User.vue
@@ -8,11 +8,42 @@
     </el-breadcrumb>
     <!-- 搜索区域 -->
     <div style="margin-top: 15px;">
-      <el-input placeholder="请输入内容" v-model="userKey" class="input-with-select" style='width:300px;margin-right:15px'>
+      <el-input
+        placeholder="请输入内容"
+        v-model="userKey"
+        class="input-with-select"
+        style="width:300px;margin-right:15px"
+      >
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
       <el-button type="success" plain>添加用户</el-button>
     </div>
+    <!-- 表格 -->
+    <el-table :data="tableData" border style="width: 100%;margin-top:15px">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址" width="300"></el-table-column>
+      <el-table-column label="状态"  width="120">
+        <template slot-scope="scope">
+          <el-switch v-model="value2" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+          <!-- 插槽：匿名插槽，具名插槽，数据插槽 -->
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button type="info" icon="el-icon-edit"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="分配角色" placement="top">
+            <el-button type="success" icon="el-icon-share"></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button type="warning" icon="el-icon-delete"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
@@ -20,11 +51,39 @@ import { getAllList } from '@/api/users.js'
 export default {
   data () {
     return {
+      value2: true,
       query: '',
       pagenum: 1,
       pagesize: 10,
       // 搜索关键字
-      userKey: ''
+      userKey: '',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleEdit (obj) {
+      console.log(obj)
     }
   },
   // 页面加载完成就去获取用户列表数据

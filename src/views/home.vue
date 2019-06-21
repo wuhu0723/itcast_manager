e466fdbf4b01f54e5eb51842db9dc4d01a9da1fe
wuhu <wuhu@itcast.cn>
2019-06-21 08:39:44 +0800

    实现了登陆业务，下一步需要完成首页

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,20 +2,7 @@
     <div class="home">首页</div>
 </template>
 <script>
-import { getAllList } from '@/api/users.js'
 export default {
-  mounted () {
-    getAllList({
-      query: '',
-      pagenum: 1,
-      pagesize: 10
-    }).then(res => {
-      console.log(res)
-    })
-      .catch(err => {
-        console.log(err)
-      })
-  }
 }
 </script>
 <style lang="less" scoped>

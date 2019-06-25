968312ea5dcdbcda9d8e3b73883c5895e5ba0fa9
wuhu <wuhu@itcast.cn>
2019-06-25 12:07:25 +0800

    加载树形组件的默认选中

--- src/views/rights/roles.vue
+++ src/views/rights/roles.vue
@@ -13,23 +13,44 @@
         <!-- 展开的时候，template模板中的结构就是展开行的内容 -->
         <template slot-scope="scope">
           <!-- 遍历数据行对象的children -->
-          <el-row v-for="first in scope.row.children" :key="first.id" style='margin-bottom:10px;border-bottom:1px dashed #ccc'>
+          <el-row
+            v-for="first in scope.row.children"
+            :key="first.id"
+            style="margin-bottom:10px;border-bottom:1px dashed #ccc"
+          >
             <el-col :span="4">
-              <el-tag closable type="success"  @close='deleteright(scope.row,first.id)' v-if='first.children.length !== 0'>{{first.authName}}</el-tag>
+              <el-tag
+                closable
+                type="success"
+                @close="deleteright(scope.row,first.id)"
+                v-if="first.children.length !== 0"
+              >{{first.authName}}</el-tag>
             </el-col>
             <el-col :span="20">
-              <el-row v-for='second in first.children' :key='second.id' style='margin-bottom:10px;' >
-                <el-col :span='4'>
-                  <el-tag closable type="info"  @close='deleteright(scope.row,second.id)'  v-if='second.children.length !== 0'>{{second.authName}}</el-tag>
-                  </el-col>
-                <el-col :span='20'>
-                  <el-tag closable type="danger" v-for='third in second.children' :key='third.id' style='margin:0 4px 4px 0' @close='deleteright(scope.row,third.id)'>{{third.authName}}</el-tag>
+              <el-row v-for="second in first.children" :key="second.id" style="margin-bottom:10px;">
+                <el-col :span="4">
+                  <el-tag
+                    closable
+                    type="info"
+                    @close="deleteright(scope.row,second.id)"
+                    v-if="second.children.length !== 0"
+                  >{{second.authName}}</el-tag>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag
+                    closable
+                    type="danger"
+                    v-for="third in second.children"
+                    :key="third.id"
+                    style="margin:0 4px 4px 0"
+                    @close="deleteright(scope.row,third.id)"
+                  >{{third.authName}}</el-tag>
                 </el-col>
               </el-row>
             </el-col>
           </el-row>
           <el-row>
-            <el-col :span="24" v-if='scope.row.children.length === 0'>没有任何的权限数据，请先添加</el-col>
+            <el-col :span="24" v-if="scope.row.children.length === 0">没有任何的权限数据，请先添加</el-col>
           </el-row>
         </template>
       </el-table-column>
@@ -43,7 +64,7 @@
             <el-button type="info" icon="el-icon-edit"></el-button>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="角色授权" placement="top">
-            <el-button type="success" icon="el-icon-share"></el-button>
+            <el-button type="success" icon="el-icon-share" @click="showGrantDialog(scope.row)"></el-button>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="删除" placement="top">
             <el-button type="warning" icon="el-icon-delete"></el-button>
@@ -51,33 +72,89 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <!-- 分配权限弹出框 -->
+    <el-dialog title="角色授权" :visible.sync="grantdialogFormVisible">
+      <div class="box">
+        <el-tree
+        :data="rightList"
+        show-checkbox
+        node-key="id"
+        :default-expand-all='true'
+        :default-checked-keys="checkedArr"
+        :props="defaultProps"
+      ></el-tree>
+      </div>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="grantdialogFormVisible = false">取 消</el-button>
+        <el-button type="primary">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
 import { getAllRoleList, deleteRightById } from '@/api/roles.js'
+import { getAllRightList } from '@/api/rights.js'
 export default {
   data () {
     return {
+      // 当前角色权限id数组
+      checkedArr: [],
+      // 所有权限数据
+      rightList: [],
+      // 树形组件节点配置
+      defaultProps: {
+        // 如果找下一级数据
+        children: 'children',
+        // 设置用于展示的标签属性
+        label: 'authName'
+      },
+      grantdialogFormVisible: false,
       roleList: []
     }
   },
   methods: {
+    // 打开授权对话框
+    showGrantDialog (row) {
+      this.grantdialogFormVisible = true
+      // 获取所有权限数据
+      getAllRightList('tree')
+        .then(res => {
+          console.log(res)
+          this.rightList = res.data.data
+        })
+      // 获取当前角色所拥有的所有权限id
+      // 先将上一个角色的权限id数组清空
+      this.checkedArr.length = 0
+      row.children.forEach((first) => {
+        if (first.children.length > 0) {
+          // 遍历二级权限
+          first.children.forEach(second => {
+            if (second.children.length > 0) {
+              // 遍历三级权限
+              second.children.forEach(third => {
+                this.checkedArr.push(third.id)
+              })
+            }
+          })
+        }
+      })
+    },
     // 删除指定权限
     deleteright (row, rightid) {
-      deleteRightById(row.id, rightid)
-        .then(res => {
-          console.log('--------------')
-          console.log(res)
-          console.log('--------------')
-          if (res.data.meta.status === 200) {
-            this.$message({
-              type: 'success',
-              message: res.data.meta.msg
-            })
-            // 数据的刷新
-            row.children = res.data.data
-          }
-        })
+      deleteRightById(row.id, rightid).then(res => {
+        console.log('--------------')
+        console.log(res)
+        console.log('--------------')
+        if (res.data.meta.status === 200) {
+          this.$message({
+            type: 'success',
+            message: res.data.meta.msg
+          })
+          // 数据的刷新
+          row.children = res.data.data
+        }
+      })
     }
   },
   mounted () {
@@ -90,4 +167,8 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.box{
+  height: 400px;
+  overflow: scroll;
+}
 </style>
